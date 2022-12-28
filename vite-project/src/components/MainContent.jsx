b842e0ebfbba078df1e02d8f266dff777cd646e8
Ron Alec <balidronalec@gmail.com>
2022-12-28 13:56:55 +0800

    get image url from data array

--- vite-project/src/components/MainContent.jsx
+++ vite-project/src/components/MainContent.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import "../Styles/mainContent.css"
+import memesData from '../data/memesData'
 
 export default function MainContent() {
+    function getMemeImage() {
+      const memes = memesData.data.memes
+      const randomMemeImage = memes[Math.floor(Math.random() * memes.length)].url
+      console.log(randomMemeImage)
+      
+      const memeImage = document.getElementById('meme-image')
+      memeImage.src = randomMemeImage
+    }
+
     return (
         <main>
-          <form action="">
+          <div className='form'>
             <input type="text" name="top-text" id="top-text" placeholder='Top Text' />
             <input type="text" name="bottom-text" id="bottom-text" placeholder='Bottom Text' />
-            <button>Get a new meme image 🖼</button>
-          </form>
+            <button onClick={getMemeImage}>Get a new meme image 🖼</button>
+          </div>
+          <div className="memeImageContainer">
+            <img src="" alt="" id="meme-image" />
+          </div>
         </main>
     )
 }
