c121c3788b2fb9ec0edc0127c14da4674efb66cc
Ron Alec <balidronalec@gmail.com>
2022-12-28 23:13:54 +0800

    getMemeImages

--- vite-project/src/components/MainContent.jsx
+++ vite-project/src/components/MainContent.jsx
@@ -4,14 +4,28 @@ import memesData from '../data/memesData'
 
 export default function MainContent() {
 
-    const [memeImage, setMemeImage] = React.useState("")
 
-    function getMemeImage() {
-      const memes = memesData.data.memes
-      const randomMemeImage = memes[Math.floor(Math.random() * memes.length)].url
-      
-      setMemeImage(randomMemeImage)
-    }
+  const [meme, setMeme] = React.useState({
+      topText: "",
+      bottomText: "",
+      randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+  
+  const [allImages, setAllImages] = React.useState(memesData)
+  
+  function getMemeImage() {
+    const memesArray = allImages.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const newURL = memesArray[randomNumber].url
+    console.log(newURL)
+    
+    setMeme(prevMeme => {
+        return {
+            ...prevMeme,
+            randomImage: newURL
+        }
+    })   
+  }
 
     return (
         <main>
@@ -21,7 +35,7 @@ export default function MainContent() {
             <button onClick={getMemeImage}>Get a new meme image 🖼</button>
           </div>
           <div className="memeImageContainer">
-            <img src={memeImage} alt="" id="meme-image" />
+            <img src={meme.randomImage} alt="meme image" id="meme-image" />
           </div>
         </main>
     )
